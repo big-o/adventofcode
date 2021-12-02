76376a98cd9573143c7bcb70e4f8989d0f19ed78
big-o <big-o@users.noreply.github.com>
2021-12-02 10:47:21 +0000

    day2

---
+++ day02.py
@@ -0,0 +1,42 @@
+def part_a(fh):
+    fwd = 0
+    up = 0
+    down = 0
+    for row in fh:
+        direction, dist = row.split()
+        dist = int(dist)
+        if direction == "forward":
+            fwd += dist
+        elif direction == "down":
+            down += dist
+        else:
+            up += dist
+
+    return fwd * (down - up)
+
+
+def part_b(fh):
+    x = 0
+    y = 0
+    aim = 0
+    for row in fh:
+        direction, dist = row.split()
+        dist = int(dist)
+        if direction == "forward":
+            x += dist
+            y += aim * dist
+        elif direction == "down":
+            aim += dist
+        else:
+            aim -= dist
+
+    return x * y
+
+
+if __name__ == "__main__":
+    filename = "data/02"
+    with open(filename) as fh:
+        print(part_a(fh))
+
+    with open(filename) as fh:
+        print(part_b(fh))
