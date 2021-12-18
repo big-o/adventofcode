f504fd27ef5e0d684fb3915c9cb1ffbdaf529843
big-o <big-o@users.noreply.github.com>
2021-12-18 11:23:07 +0000

    day 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,15 +1,18 @@
+from collections import deque
+
+
 def main(fh, window_size=1):
     it = map(int, fh)
     try:
-        window = [next(it) for _ in range(window_size)]
+        window = deque([next(it) for _ in range(window_size)])
     except StopIteration:
         raise ValueError(f"Window size {window_size} must be smaller than input.")
 
     answer = 0
     for num in it:
-        if num > window[0]:
+        if num > window.popleft():
             answer += 1
-        window = window[1:] + [num]
+        window.append(num)
 
     return answer
 
